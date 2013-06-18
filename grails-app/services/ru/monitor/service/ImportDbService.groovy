4831ef1d980ede685de92363aaa7c37470214113
mmkoshelev <mmkoshelev@yandex.ru>
2013-06-18 22:03:25 +0400

    Базовые наброски сервиса импорта БД (эталонные данные)

--- grails-app/services/ru/monitor/service/ImportDbService.groovy
+++ grails-app/services/ru/monitor/service/ImportDbService.groovy
@@ -16,21 +16,27 @@ class ImportDbService {
     def importDb(ServerItem serverItem, String filePath) {
         Sql sql = SQLiteConnectionFactory.getConnection(filePath)
         try {
-            def monitorGroups = importMonitorGroup(sql)
-            def checkRuns = importCheckRun(sql)
+            def monitorGroups = getMonitorGroups(sql)
+            def checkRuns = getCheckRuns(sql)
+            def monitorItems = getMonitorItems(sql)
+            def patts = getPatts(sql)
+            def etalonDirs = getEtalonDirs(sql)
+            def etalonFiles = getEtalonFiles(sql)
+            def etalonAces = getEtalonAces(sql)
         } finally {
             sql.close()
         }
     }
 
-    private static importMonitorGroup(Sql sql) {
+    private static getMonitorGroups(Sql sql) {
         def list = []
         sql.eachRow("SELECT grpid, grpname FROM mvz_group") {
             def row = it.toRowResult()
             row.groups = []
-            sql.eachRow("SELECT g.grpname " +
-                        "FROM mvz_link gl INNER JOIN  mvz_group g ON gl.grp2 = g.grpid " +
-                        "WHERE gl.grp1 = ?", [row.grpid]) {
+            sql.eachRow("""SELECT g.grpname
+                           FROM mvz_link gl INNER JOIN  mvz_group g
+                               ON gl.grp2 = g.grpid
+                           WHERE gl.grp1 = ?""", [row.grpid]) {
                 row.groups << it.grpname
             }
             list << row
@@ -39,7 +45,7 @@ class ImportDbService {
         return list
     }
 
-    private static importCheckRun(Sql sql) {
+    private static getCheckRuns(Sql sql) {
         def list = []
         sql.eachRow("SELECT runid, grpid FROM mvz_chkrun") {
             list << it.toRowResult()
@@ -48,4 +54,50 @@ class ImportDbService {
         return list
     }
 
+    private static getMonitorItems(Sql sql) {
+        def list = []
+        sql.eachRow("SELECT grpid, ipos, path FROM mvz_item") {
+            list << it.toRowResult()
+        }
+
+        return list
+    }
+
+    private static getPatts(Sql sql) {
+        def list = []
+        sql.eachRow("SELECT grpid, ipos, pattern, flags FROM mvz_patt") {
+            list << it.toRowResult()
+        }
+
+        return list
+    }
+
+    private static getEtalonDirs(Sql sql) {
+        def list = []
+        sql.eachRow("SELECT dirid, grpid, dname FROM mvz_dir") {
+            list << it.toRowResult()
+        }
+
+        return list
+    }
+
+    private static getEtalonFiles(Sql sql) {
+        def list = []
+        sql.eachRow("SELECT eid, dirid, fname, fhash, fsize FROM mvz_entry") {
+            list << it.toRowResult()
+        }
+
+        return list
+    }
+
+    private static getEtalonAces(Sql sql) {
+        def list = []
+        sql.eachRow("""SELECT a.eid, a.acemode, 0 as diff, a.acevalue, t.uname, NULL as trustee
+                       FROM mvz_eace a LEFT OUTER JOIN mvz_trustee t
+                           ON a.trustee = t.uid""") {
+            list << it.toRowResult()
+        }
+
+        return list
+    }
 }
