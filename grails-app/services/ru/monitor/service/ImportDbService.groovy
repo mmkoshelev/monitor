31b9fe80372dca6c0049c841d42614f9ac94592c
mmkoshelev <mmkoshelev@yandex.ru>
2013-06-19 20:26:28 +0400

    Вынес чтение данных SQLite в отдельный сервис

--- grails-app/services/ru/monitor/service/ImportDbService.groovy
+++ grails-app/services/ru/monitor/service/ImportDbService.groovy
@@ -13,115 +13,24 @@ class ImportDbService {
 
     static transactional = false
 
+    def readDbService
+
     def importDb(ServerItem serverItem, String filePath) {
         Sql sql = SQLiteConnectionFactory.getConnection(filePath)
         try {
-            def monitorGroups = getMonitorGroups(sql)
-            def monitorItems = getMonitorItems(sql)
-            def patts = getPatts(sql)
-            def etalonDirs = getEtalonDirs(sql)
-            def etalonFiles = getEtalonFiles(sql)
-            def etalonAces = getEtalonAces(sql)
-            def checkRuns = getCheckRuns(sql)
-            def checkFiles = getCheckFiles(sql)
-            def checkAces = getCheckAces(sql)
+            def monitorGroups = readDbService.getMonitorGroups(sql)
+            def monitorItems = readDbService.getMonitorItems(sql)
+            def patts = readDbService.getPatts(sql)
+            def etalonDirs = readDbService.getEtalonDirs(sql)
+            def etalonFiles = readDbService.getEtalonFiles(sql)
+            def etalonAces = readDbService.getEtalonAces(sql)
+            def checkRuns = readDbService.getCheckRuns(sql)
+            def checkFiles = readDbService.getCheckFiles(sql)
+            def checkAces = readDbService.getCheckAces(sql)
         } finally {
             sql.close()
         }
     }
 
-    private static getMonitorGroups(Sql sql) {
-        def list = []
-        sql.eachRow("SELECT grpid, grpname FROM mvz_group") {
-            def row = it.toRowResult()
-            row.groups = []
-            sql.eachRow("""SELECT g.grpname
-                           FROM mvz_link gl INNER JOIN  mvz_group g
-                               ON gl.grp2 = g.grpid
-                           WHERE gl.grp1 = ?""", [row.grpid]) {
-                row.groups << it.grpname
-            }
-            list << row
-        }
 
-        return list
-    }
-
-    private static getCheckRuns(Sql sql) {
-        def list = []
-        sql.eachRow("SELECT runid, grpid FROM mvz_chkrun") {
-            list << it.toRowResult()
-        }
-
-        return list
-    }
-
-    private static getMonitorItems(Sql sql) {
-        def list = []
-        sql.eachRow("SELECT grpid, ipos, path FROM mvz_item") {
-            list << it.toRowResult()
-        }
-
-        return list
-    }
-
-    private static getPatts(Sql sql) {
-        def list = []
-        sql.eachRow("SELECT grpid, ipos, pattern, flags FROM mvz_patt") {
-            list << it.toRowResult()
-        }
-
-        return list
-    }
-
-    private static getEtalonDirs(Sql sql) {
-        def list = []
-        sql.eachRow("SELECT dirid, grpid, dname FROM mvz_dir") {
-            list << it.toRowResult()
-        }
-
-        return list
-    }
-
-    private static getEtalonFiles(Sql sql) {
-        def list = []
-        sql.eachRow("SELECT eid, dirid, fname, fhash, fsize FROM mvz_entry") {
-            list << it.toRowResult()
-        }
-
-        return list
-    }
-
-    private static getEtalonAces(Sql sql) {
-        def list = []
-        sql.eachRow("""SELECT a.eid, a.acemode, 0 as diff, a.acevalue, t.uname, NULL as trustee
-                       FROM mvz_eace a LEFT OUTER JOIN mvz_trustee t
-                           ON a.trustee = t.uid""") {
-            list << it.toRowResult()
-        }
-
-        return list
-    }
-
-    private static getCheckFiles(Sql sql) {
-        def list = []
-        sql.eachRow("SELECT ceid, dirid, fname, status FROM mvz_chkent") {
-            list << it.toRowResult()
-        }
-
-        return list
-    }
-
-    private static getCheckAces(Sql sql) {
-        def list = []
-        sql.eachRow("""SELECT a.ceid, a.acemode, a.cdiff, a.acevalue, t.uname, ct.uname as trustee
-                       FROM mvz_chkace a LEFT OUTER JOIN mvz_trustee t
-                           ON a.trustee = t.uid
-                       LEFT OUTER JOIN mvz_chktrust ct
-                           ON a.trustee = ct.uid""") {
-            list << it.toRowResult()
-        }
-
-        return list
-    }
 }
---
+++ grails-app/services/ru/monitor/service/ReadDbService.groovy
@@ -0,0 +1,108 @@
+package ru.monitor.service
+
+import groovy.sql.Sql
+
+/**
+ * Чтение данных SQLite DB
+ *
+ * @since 0.1
+ */
+class ReadDbService {
+
+    static transactional = false
+
+    def getMonitorGroups(Sql sql) {
+        def list = []
+        sql.eachRow("SELECT grpid, grpname FROM mvz_group") {
+            def row = it.toRowResult()
+            row.groups = []
+            sql.eachRow("""SELECT g.grpname
+                           FROM mvz_link gl INNER JOIN  mvz_group g
+                               ON gl.grp2 = g.grpid
+                           WHERE gl.grp1 = ?""", [row.grpid]) {
+                row.groups << it.grpname
+            }
+            list << row
+        }
+
+        return list
+    }
+
+    def getCheckRuns(Sql sql) {
+        def list = []
+        sql.eachRow("SELECT runid, grpid FROM mvz_chkrun") {
+            list << it.toRowResult()
+        }
+
+        return list
+    }
+
+    def getMonitorItems(Sql sql) {
+        def list = []
+        sql.eachRow("SELECT grpid, ipos, path FROM mvz_item") {
+            list << it.toRowResult()
+        }
+
+        return list
+    }
+
+    def getPatts(Sql sql) {
+        def list = []
+        sql.eachRow("SELECT grpid, ipos, pattern, flags FROM mvz_patt") {
+            list << it.toRowResult()
+        }
+
+        return list
+    }
+
+    def getEtalonDirs(Sql sql) {
+        def list = []
+        sql.eachRow("SELECT dirid, grpid, dname FROM mvz_dir") {
+            list << it.toRowResult()
+        }
+
+        return list
+    }
+
+    def getEtalonFiles(Sql sql) {
+        def list = []
+        sql.eachRow("SELECT eid, dirid, fname, fhash, fsize FROM mvz_entry") {
+            list << it.toRowResult()
+        }
+
+        return list
+    }
+
+    def getEtalonAces(Sql sql) {
+        def list = []
+        sql.eachRow("""SELECT a.eid, a.acemode, 0 as diff, a.acevalue, t.uname, NULL as trustee
+                       FROM mvz_eace a LEFT OUTER JOIN mvz_trustee t
+                           ON a.trustee = t.uid""") {
+            list << it.toRowResult()
+        }
+
+        return list
+    }
+
+    def getCheckFiles(Sql sql) {
+        def list = []
+        sql.eachRow("SELECT ceid, dirid, fname, status FROM mvz_chkent") {
+            list << it.toRowResult()
+        }
+
+        return list
+    }
+
+    def getCheckAces(Sql sql) {
+        def list = []
+        sql.eachRow("""SELECT a.ceid, a.acemode, a.cdiff, a.acevalue, t.uname, ct.uname as trustee
+                       FROM mvz_chkace a LEFT OUTER JOIN mvz_trustee t
+                           ON a.trustee = t.uid
+                       LEFT OUTER JOIN mvz_chktrust ct
+                           ON a.trustee = ct.uid""") {
+            list << it.toRowResult()
+        }
+
+        return list
+    }
+}
